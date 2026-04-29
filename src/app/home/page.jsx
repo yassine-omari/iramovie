9c1a4eb32052ddb1da0339b4a26bc52341da5a7c
yassine-omari <yo0546436@gmail.com>
2026-04-29 09:10:22 +0100

    fix bug

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server";
+import { createClient } from "../../../lib/server";
 import { redirect } from "next/navigation";
 import Navbar from "../components/navbar";
 
